ece69c58b2d74669cf4ea4631f2b8da79cb01f50
I-AM-CHAMI <88298450+chamiofficial@users.noreply.github.com>
2025-04-17 15:22:43 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,7 +40,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~Cd8GCDDC#tuM-M23Vv7UJu8HgMNgliHmVvB0mAUT_wbm90miO1hg",
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~DBMCXDgI#kHpi4NjFpcf8tjBS2YBqCiVB73Ieavw3Jd1orwGWSbg",
     GITHUB_TOKEN: process.env.GITHUB_TOKEN || "FVdikXn7mHjOitVZ6nqvCNQ9uooInl3ITI9X",
     GITHUB_USERNAME: process.env.GITHUB_USERNAME || "chamiofficial",
     SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94775512050","94741230539","94701454728"],
